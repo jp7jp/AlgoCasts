99e329b2bec357e22369e6247fbbe616251293a7
JP Barbosa <jp@jp7.com.br>
2017-11-24 17:19:51 -0200

    Linked Lists

--- exercises/linkedlist/index.js
+++ exercises/linkedlist/index.js
@@ -2,8 +2,144 @@
 // Implement classes Node and Linked Lists
 // See 'directions' document
 
-class Node {}
+class Node {
 
-class LinkedList {}
+  constructor(data, next = null) {
+    this.data = data;
+    this.next = next;
+  }
+
+}
+
+class LinkedList {
+
+  constructor() {
+    this.head = null;
+  }
+
+  insertFirst(data) {
+    this.head = new Node(data, this.head);
+    this.counter++;
+  }
+
+  size() {
+    let node = this.head;
+    let counter = 0;
+    while (node) {
+      node = node.next;
+      counter++;
+    }
+    return counter;
+  }
+
+  getFirst() {
+    return this.head;
+  }
+
+  getLast() {
+
+    if (!this.head) {
+      return null;
+    }
+
+    let node = this.head;
+    while (node.next) {
+      node = node.next;
+    }
+    return node;
+  }
+
+  clear() {
+    this.head = null;
+  }
+
+  removeFirst() {
+
+    if (!this.head) {
+      return;
+    }
+
+    this.head = this.head.next;
+  }
+
+  removeLast() {
+
+    if (!this.head) {
+      return;
+    }
+
+    if (!this.head.next) {
+      this.head = null;
+      return;
+    }
+
+    let previous = this.head;
+    let node = this.head.next;
+    while (node.next) {
+      previous = node;
+      node = node.next;
+    }
+    previous.next = null
+  }
+
+  insertLast(data) {
+    const node = new Node(data);
+    const last = this.getLast();
+    if (last) {
+      last.next = node;
+    } else {
+      this.head = node;
+    }
+  }
+
+  getAt(index) {
+    let node = this.head
+    let counter = 0;
+    while (node) {
+      if (counter == index) {
+        return node;
+      }
+      node = node.next;
+      counter++;
+    }
+
+    return null;
+  }
+
+  removeAt(index) {
+
+    if (!this.head) {
+      return;
+    }
+
+    if (index === 0) {
+      this.head = this.head.next;
+      return;
+    }
+
+    const previous = this.getAt(index - 1);
+    if (!previous || !previous.next) {
+      return;
+    }
+
+    previous.next = previous.next.next;
+
+  }
+
+  insertAt(data, index) {
+
+    if (!this.head || index === 0) {
+      this.insertFirst(data);
+      return;
+    }
+
+    const previous = this.getAt(index - 1) || this.getLast();
+
+    previous.next = new Node(data, previous.next);
+    return;
+
+  }
+
+}
 
 module.exports = { Node, LinkedList };
