e4ef65612d225be988db748a91e5c1d6e6d50de8
JP Barbosa <jp@jp7.com.br>
2017-11-27 14:48:26 -0200

    Additional Linked List exercises

--- exercises/fromlast/index.js
+++ exercises/fromlast/index.js
@@ -11,6 +11,14 @@
 //    list.insertLast('d');
 //    fromLast(list, 2).data // 'b'
 
-function fromLast(list, n) {}
+function fromLast(list, n) {
+  slow = list.getFirst();
+  fast = list.getAt(n);
+  while (fast.next) {
+    slow = slow.next;
+    fast = fast.next;
+  }
+  return slow;
+}
 
 module.exports = fromLast;
